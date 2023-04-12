fe07881a2c40087ccb0f5d30d94d2328e8484d93
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-04-12 19:27:10 +0430

    5- create view function for list and detail

--- website/views.py
+++ website/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+
+def post_list(request):
+
+	posts=Post.published.all()
+	return render(request,'website/post/list.html',context={'post':posts})
+
+
+def post_detail(request):
+
+	post=get_object_or_404(Post,slug=slug,
+								status='p',
+								publish__yeaer=year,
+								publish__month=month,
+								publisih__day=day,
+
+												)
+
+	return render(request,'website/post/detail.html',context={'post':post})
