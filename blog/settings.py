26519f7e4bb76ca11a7735dc89d39a46eb2d7eda
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-04-15 19:40:28 +0430

    15- add sitemap to website

--- blog/settings.py
+++ blog/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'taggit'
 ]
 
--- website/templatetags/blog_tags.py
+++ website/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.db.models import Count
 from django.utils.safestring import mark_safe
+from django.utils.html import conditional_escape 
 import markdown
 from website.models import Post
 register=template.Library()
